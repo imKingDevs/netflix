4daf79aec2a1e2c452518787672268e13347036b
Dev <124387485+imKingDevs@users.noreply.github.com>
2023-08-02 10:31:27 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+    <>
+        <h1>ShowFlix</h1>
+        <h2>List of Top 5 Show</h2>
+        <ul>
+            <li>Lucifer</li>
+            <li>Tommorow</li>
+            <li>The Last Kingdom</li>
+            <li>Hellbound</li>
+            <li>Lost in Space</li>
+        </ul>
+    </>,
+    document.querySelector('#root'));
